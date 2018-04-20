232d55c3226ed9265ecf8b5651a045fc577bca38
linux <iantonini.m@gmail.com>
2018-04-20 13:11:50 -0300

    exibir imagens da api

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,21 @@
-var request = require('request');
+const Request = require("request");
+const Url = "https://raw.githubusercontent.com/iantonini/MyAppNodeNW/master/exemplo.html";
+// const Url = "https://private-7cf60-4youseesocialtest.apiary-mock.com/timeline";
 
-request('https://private-7cf60-4youseesocialtest.apiary-mock.com/timeline', function (error, response, body) {
-  console.log('Status:', response.statusCode);
-  console.log('Headers:', JSON.stringify(response.headers));
-  console.log('Response:', body);
+Request.get(Url, (error, response, body) => {
+	if(error || response.statusCode != 200){
+		console.dir(error);
+		throw "Erro ao receber as informações da API";
+	}
+
+    var Jresponse = JSON.parse(body);
+
+    for(var i=0; i < Jresponse.length; i++){
+    	
+    	var html = '<p><image class="col-md-3" src="' + Jresponse[i].thumbnail + '"/></p>';
+		$('#json').append(html);
+    }
 });
+
+
+
