4e8ec299e6ca1fca24dc77f97444c871c4ff5904
linux <iantonini.m@gmail.com>
2018-04-22 21:13:29 -0300

    Add nw-builder e atualizando documentação

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
     - curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
     - sudo apt-get install -y nodejs
     - sudo apt-get install -y build-essential
-    
+
     Verificar as versões instaladas
     - node -v
     - npm -v
@@ -19,37 +19,52 @@
   ### Criar Projeto
     Criar Diretório para o APP
     - mkdir MyAppNodeNW
-  
-  Acessar o novo diretório
-  - cd MyAppNodeNW
-  
-  Clonar o repositório
-  - git clone https://github.com/iantonini/MyAppNodeNW.git .
-  
-  Instalar dependências do NodeJS
-  - npm i --production
+
+    Acessar o novo diretório
+    - cd MyAppNodeNW
+
+    Clonar o repositório
+    - git clone https://github.com/iantonini/MyAppNodeNW.git .
+
+    Instalar dependências do NodeJS
+    - npm i --production
 
   ### Executar Aplicativo
     Acessar o diretório do aplicativo
     - cd MyAppNodeNW
-  
-  Executar NW para iniciar APP
-  - node_modulo/.bin/nw . 
+
+    Executar NW para iniciar APP
+    - node_modulo/.bin/nw . 
 
   ### Complilar Aplicativo (Linux)
-    Abrir terminal usando atalho
-  - Ctr+Alt+T
-  
-  Acessar o diretório do aplicativo
+    Acessar o diretório do aplicativo
     - cd MyAppNodeNW
-  
-  Criar arquivo ZIP de todo o conteudo do diretório
-  - zip -r MyAppNW.nw *
-  
-  Gerar arquivo executavel
-  - cat `which nw` app.nw > app && chmod +x app 
+
+    Executar o nw-builder de acordo com a plataforma desejada
+        Linux32
+        - node_modules/.bin/nwbuild -plinux32 .
+
+        Linux64
+        - node_modules/.bin/nwbuild -plinux64 .
+
+        Win32
+        - node_modules/.bin/nwbuild -pwin32 .
+
+        Win64
+        - node_modules/.bin/nwbuild -pwin64 .
+
+        OSX32
+        - node_modules/.bin/nwbuild -posx32 .
+
+        OSX64
+        - node_modules/.bin/nwbuild -posx64 .
+
+    Gerar arquivo executavel
+    - cat `which nw` app.nw > app && chmod +x app 
 
 
 ## Links Referências
  - https://nodejs.org/en/
  - https://nwjs.io/
+ - https://github.com/nwjs-community/nw-builder
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,6 @@
 const Request = require("request");
-const Url = "https://raw.githubusercontent.com/iantonini/MyAppNodeNW/master/exemplo.html";
-// const Url = "https://private-7cf60-4youseesocialtest.apiary-mock.com/timeline";
-body
+//const Url = "https://raw.githubusercontent.com/iantonini/MyAppNodeNW/master/exemplo.html";
+const Url = "https://private-7cf60-4youseesocialtest.apiary-mock.com/timeline";
 
 // Iniciando Filtros
 var Filtros = [];
@@ -14,65 +13,65 @@ partes.forEach(function(parte){
 });
 
 Request.get(Url, (error, response, body) => {
-	if(error || response.statusCode != 200){
-		console.dir(error);
-		throw "Erro ao receber as informações da API";
-	}
+    if(error || response.statusCode != 200){
+        console.dir(error);
+        throw "Erro ao receber as informações da API";
+    }
 
     var Jresponse = JSON.parse(body);
 
     for(var i=0; i < Jresponse.length; i++){
 
-		// Filtro de Categorias
-    	if(Filtros.indexOf('category_' + Jresponse[i].category) === -1){
-    		Filtros.push('category_' + Jresponse[i].category);
-    		var filtro_categoria = '<a class="dropdown-item" href="./index.html?search=category_' + Jresponse[i].category + '">' + Jresponse[i].category + '</a>';
-    		$('#category').append(filtro_categoria);
-    	}
+        // Filtro de Categorias
+        if(Filtros.indexOf('category_' + Jresponse[i].category) === -1){
+            Filtros.push('category_' + Jresponse[i].category);
+            var filtro_categoria = '<a class="dropdown-item" href="./index.html?search=category_' + Jresponse[i].category + '">' + Jresponse[i].category + '</a>';
+            $('#category').append(filtro_categoria);
+        }
 
-		// Filtro de Tipos de Mídia
-		if(Filtros.indexOf('type_' + Jresponse[i].type) === -1){
-			Filtros.push('type_' + Jresponse[i].type);
-			var filtro_tipo_midia = '<a class="dropdown-item" href="./index.html?search=type_' + Jresponse[i].type + '">' + Jresponse[i].type + '</a>';
-			$('#type').append(filtro_tipo_midia);
-		}
+        // Filtro de Tipos de Mídia
+        if(Filtros.indexOf('type_' + Jresponse[i].type) === -1){
+            Filtros.push('type_' + Jresponse[i].type);
+            var filtro_tipo_midia = '<a class="dropdown-item" href="./index.html?search=type_' + Jresponse[i].type + '">' + Jresponse[i].type + '</a>';
+            $('#type').append(filtro_tipo_midia);
+        }
 
-		// Conteudo da APIbody
-		var html = '<div class="jumbotron">\
-						<div class="row col-md-12">\
-							<div class="col-md-12">\
-								Category: ' + Jresponse[i].category + '\
-							</div>\
-						</div>\
-						<div class="row col-md-12">\
-							<div class="row col-md-12">\
-								<div class="col-md-5">\
-									<image class="col-md-12" src="' + Jresponse[i].thumbnail + '"/>\
-								</div>\
-								<div class="col-md-7">\
-									<div class="col-md-12">\
-										<h6>Title:</h6> ' + Jresponse[i].title + '\
-									</div>\
-									<div class="col-md-12">\
-										<h6>Type:</h6> ' + Jresponse[i].type + '\
-									</div>\
-									<div class="col-md-12">\
-										<h6>Description:</h6> ' + Jresponse[i].description + '\
-									</div>\
-									<div class="col-md-12">\
-										<h6>File:</h6> ' + Jresponse[i].file + '\
-									</div>\
-								</div>\
-							</div>\
-						</div>\
-					</div>';
+        // Conteudo da APIbody
+        var html = '<div class="jumbotron">\
+                        <div class="row col-md-12">\
+                            <div class="col-md-12">\
+                                Category: ' + Jresponse[i].category + '\
+                            </div>\
+                        </div>\
+                        <div class="row col-md-12">\
+                            <div class="row col-md-12">\
+                                <div class="col-md-5">\
+                                    <image class="col-md-12" src="' + Jresponse[i].thumbnail + '"/>\
+                                </div>\
+                                <div class="col-md-7">\
+                                    <div class="col-md-12">\
+                                        <h6>Title:</h6> ' + Jresponse[i].title + '\
+                                    </div>\
+                                    <div class="col-md-12">\
+                                        <h6>Type:</h6> ' + Jresponse[i].type + '\
+                                    </div>\
+                                    <div class="col-md-12">\
+                                        <h6>Description:</h6> ' + Jresponse[i].description + '\
+                                    </div>\
+                                    <div class="col-md-12">\
+                                        <h6>File:</h6> ' + Jresponse[i].file + '\
+                                    </div>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </div>';
 
-		if(Query != '' && Query != 'all' && Query != undefined){
-			if(Query != 'category_' + Jresponse[i].category && Query != 'type_' + Jresponse[i].type){
-				continue;
-			}
-		}
-		$('#json').append(html);
+        if(Query != '' && Query != 'all' && Query != undefined){
+            if(Query != 'category_' + Jresponse[i].category && Query != 'type_' + Jresponse[i].type){
+                continue;
+            }
+        }
+        $('#json').append(html);
     }
 });
 
